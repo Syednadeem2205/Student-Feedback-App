7142022f468f7ddd80a7380fbaafa68dfc62131c
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-15 00:40:07 +0530

    Update courseService.js

--- frontend/src/services/courseService.js
+++ frontend/src/services/courseService.js
@@ -1,7 +1,5 @@
 import api from "../api";
-
-export const getCourses = () => api.get("/courses");
-
-export const addCourse = (data) => api.post("/courses", data);
-
+export const getCourses = () => api.get('/courses');
+export const createCourse = (data) => api.post('/courses', data);
+export const updateCourse = (id, data) => api.put(`/courses/${id}`, data);
 export const deleteCourse = (id) => api.delete(`/courses/${id}`);
