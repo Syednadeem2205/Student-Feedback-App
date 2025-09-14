ad132250e670dde888182c14970e28715eea3ebb
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-15 00:43:20 +0530

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,5 +1,16 @@
-import api from "../api";
+import api from '../api';
 
-export const getUsers = () => api.get("/users");
+// Fetch all users
+export const getUsers = () => api.get('/users');
 
+// Fetch a user by ID
 export const getUserById = (id) => api.get(`/users/${id}`);
+
+// Update current user profile
+export const updateProfile = (data) => api.put('/users/me', data);
+
+// Upload avatar for current user
+export const uploadAvatar = (formData) =>
+  api.post('/users/me/avatar', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
