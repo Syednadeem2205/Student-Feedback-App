ff98062eab55498cb7cc0351c82f03ede60b4d87
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-14 18:29:29 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import MyFeedbacks from './pages/MyFeedback';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
+
+export default function App(){
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
+          <Route path="/my-feedbacks" element={<PrivateRoute><MyFeedbacks/></PrivateRoute>} />
+          <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>} />
+        </Routes>
+      </div>
+    </>
+  );
+}
