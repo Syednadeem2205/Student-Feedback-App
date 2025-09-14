b6f9c1df362c200a15e752c229052d7fb47b9fb5
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-15 00:19:47 +0530

    Create authService.js

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,7 @@
+import api from "../api";
+
+export const signup = (data) => api.post("/auth/signup", data);
+
+export const login = (data) => api.post("/auth/login", data);
+
+export const getProfile = () => api.get("/auth/profile");
