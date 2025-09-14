7ff305cc4f41b8f8573a2ef78e08aa2a3aa9e51d
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-14 18:56:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import MyFeedbacks from './pages/MyFeedback';
 import Profile from './pages/Profile';
+import AdminDashboard from './pages/AdminDashboard';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -20,8 +21,11 @@ export default function App(){
           <Route path="/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>} />
           <Route path="/my-feedbacks" element={<PrivateRoute><MyFeedbacks/></PrivateRoute>} />
           <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>} />
+          <Route path="/admin" element={<PrivateRoute><AdminDashboard/></PrivateRoute>} />
+          <Route path="/admin/courses" element={<PrivateRoute><AdminCourses/></PrivateRoute>} />
         </Routes>
       </div>
     </>
   );
 }
+
