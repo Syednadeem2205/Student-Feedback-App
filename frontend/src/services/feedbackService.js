16791e52886872060770aa3ad08c2b7f75537dfa
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-15 13:53:52 +0530

    Update feedbackService.js

--- frontend/src/services/feedbackService.js
+++ frontend/src/services/feedbackService.js
@@ -1,13 +1,25 @@
 import api from '../api';
 
 // Submit new feedback
-export const submitFeedback = (data) => api.post('/feedback', data);
+export const submitFeedback = async (data) => {
+  const res = await api.post('/feedback', data);
+  return res.data;
+};
 
-// Get all feedbacks (or optionally /mine for current user)
-export const getFeedbacks = () => api.get('/feedback');
+// Get all feedbacks
+export const getFeedbacks = async () => {
+  const res = await api.get('/feedback');
+  return res.data;
+};
 
 // Delete a feedback by ID
-export const deleteFeedback = (id) => api.delete(`/feedback/${id}`);
+export const deleteFeedback = async (id) => {
+  const res = await api.delete(`/feedback/${id}`);
+  return res.data;
+};
 
-// Optional: fetch feedbacks of the current user
-export const getMyFeedbacks = () => api.get('/feedback/mine');
+// Fetch feedbacks of the current user
+export const getMyFeedbacks = async () => {
+  const res = await api.get('/feedback/mine');
+  return res.data;
+};
