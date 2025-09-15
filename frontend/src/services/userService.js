d40cf6efc062cdc828d6a3865085bbc49c790777
Syed Nadeem Ahmed <125083669+Syednadeem2205@users.noreply.github.com>
2025-09-15 13:54:28 +0530

    Update userService.js

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,16 +1,27 @@
 import api from '../api';
 
 // Fetch all users
-export const getUsers = () => api.get('/users');
+export const getUsers = async () => {
+  const res = await api.get('/users');
+  return res.data;
+};
 
 // Fetch a user by ID
-export const getUserById = (id) => api.get(`/users/${id}`);
+export const getUserById = async (id) => {
+  const res = await api.get(`/users/${id}`);
+  return res.data;
+};
 
 // Update current user profile
-export const updateProfile = (data) => api.put('/users/me', data);
+export const updateProfile = async (data) => {
+  const res = await api.put('/users/me', data);
+  return res.data;
+};
 
 // Upload avatar for current user
-export const uploadAvatar = (formData) =>
-  api.post('/users/me/avatar', formData, {
+export const uploadAvatar = async (formData) => {
+  const res = await api.post('/users/me/avatar', formData, {
     headers: { 'Content-Type': 'multipart/form-data' },
   });
+  return res.data;
+};
